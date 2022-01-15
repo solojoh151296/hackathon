fb9671b9852e124c087437bf68545929e4a68c65
joh <joha8_1@hotmail.com>
2022-01-15 13:42:45 -0500

    Primer Vue Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <XD/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!--  -->
+    <Welcome/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+// import XD from './components/XD.vue'
+import Welcome from './components/Welcome1.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    // XD,
+    Welcome
   }
 }
 </script>
@@ -22,7 +29,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black; /*  #color letra */
   margin-top: 60px;
+  background-color: white;/*  #color fondo */
 }
 </style>
---
+++ src/components/XD.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="xd">
+    <h1>HOLIIIIIIIIIIIIIII</h1>
+    <h2> {{subtitulo}}</h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XD',
+  props: {
+    msg: String
+  },
+  data (){
+    return {
+      subtitulo: 'Soy Johana'
+    }
+  }
+}
+</script>
